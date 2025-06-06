9c7707f960b1f785299e636b3b7f96cb31f4f501
david123456858 <juandavidperaltafuentes@gmail.com>
2025-06-06 11:36:27 -0500

    vaya segunda prueba

--- prueba-2/prueba-2.js
+++ prueba-2/prueba-2.js
@@ -2,17 +2,31 @@
  * 1. de las conversiones
  * 2. ver cuantos 60 estan el los segundos dados (3000) cuantos 60 hay? ()
  */
+obcjectTime={
+        "años":0,
+        "Meses":0,
+        "Días":0,
+        "Horas":0,
+        "Minutos":0,
+        "Segundos":0
+    }
+
 const transformSeconds = (seconds)=> {
+    vectorTime = []
+    let countTime = 0
+    
     if(seconds < 0) return 'Number incorrect negative'
 
     if (seconds === 0) return 'Now'
     
-    const minuteSearched = seconds/60
+    for(let i = 0; i < 6;i++){
+        const count = seconds/60
+        if(i===0){
 
-    console.log(minuteSearched);
-    
+        } 
+    }    
 }
 
-const answer = transformSeconds(62)
+transformSeconds(62)
 
-console.log(answer);
+
