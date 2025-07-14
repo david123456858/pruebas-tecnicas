854438557cf65c0d8eedcf42c7705a581dabb428
david123456858 <juandavidperaltafuentes@gmail.com>
2025-07-13 22:06:41 -0500

    primera idea

--- prueba-3/prueba3.js
+++ prueba-3/prueba3.js
@@ -1,6 +1,13 @@
 // aqui empieza el juego de logica de esta prueba
 
+
+/**
+ * Primera idea:
+ * Es necesario hacer un array donde tenga los numeros del teclado pero solo tendria
+ * que solucionar lo del numero 0 puesto que es el unico que va a estar rodeao de nada
+ * pero de resto se puede convertir en que cada posicion.
+ */
 const findCodeDetective = (codeForFind) => {
 
-    
+
 }
